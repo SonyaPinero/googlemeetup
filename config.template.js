f083191be8f8f31da383ce59d585d5747a229750
Sonya Pinero <sonyapinero@gmail.com>
2017-02-18 20:05:16 -0500

    first api request

--- config.template.js
+++ config.template.js
@@ -2,7 +2,8 @@ import credentials from './credentials';
 
 export default {
   meetup: {
-
+    baseURL: 'https://api.meetup.com/2',
+    key: credentials.meetup.key
   },
   google: {
 
--- googlemeetup.js
+++ googlemeetup.js
@@ -1,1 +1,22 @@
-console.log('test npm script')
+import request from 'superagent';
+import config from './config';
+
+function meetup() {
+  return new Promise((resolve, reject) => {
+    request
+      .get(`${config.meetup.baseURL}/member/8468875?&sign=true&photo-host=public&page=20&key=${config.meetup.key}`)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) {
+          console.log('err', err);
+          return reject(err);
+        } else {
+          console.log('res', res);
+          return resolve(res);
+        }
+      });
+  });
+}
+
+meetup();
