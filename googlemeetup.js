6c386e5e2dd0c0f8899dc90d08cea7b3acb3de50
Sonya Pinero <sonyapinero@gmail.com>
2017-02-18 21:02:18 -0500

    meetup calendar api better choice

--- googlemeetup.js
+++ googlemeetup.js
@@ -1,10 +1,10 @@
 import request from 'superagent';
 import config from './config';
 
-function getGroupData() {
+function getEventData() {
   return new Promise((resolve, reject) => {
     request
-      .get(`${config.meetup.baseURL}/self/groups?&sign=true&fields=tech&key=${config.meetup.key}`)
+      .get(`${config.meetup.baseURL}/self/calendar?&sign=true&fields=tech&key=${config.meetup.key}`)
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
       .end((err, res) => {
@@ -19,27 +19,9 @@ function getGroupData() {
   });
 }
 
-function getEventData(eventId) {
-  return new Promise((resolve, reject) => {
-    request
-      .get(`${config.meetup.baseURL}/2/events?&sign=true&limited_events=False&event_id=${eventId}&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&key=${config.meetup.key}`)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        if (err) {
-          console.log('event err', err);
-          return reject(err);
-        } else {
-          console.log('res', res.body.results);
-          return resolve(res.body.results);
-        }
-      });
-  });
-}
 
 async function updateGoogleCalendar(){
-  //getGroupData()
-  //getEventData(237784423)
+  getEventData()
 }
 
 updateGoogleCalendar();
