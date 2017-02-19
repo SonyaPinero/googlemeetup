c19c675349f16b5b1c518037e6cee31e92e26b7a
Sonya Pinero <sonyapinero@gmail.com>
2017-02-19 14:41:48 -0500

    getting new access token and then calendar data

--- googlemeetup.js
+++ googlemeetup.js
@@ -11,40 +11,65 @@ function getEventData() {
       .set('Accept', 'application/json')
       .end((err, res) => {
         if (err) {
-          console.log('group err', err);
+          console.log('event err', err);
           return reject(err);
         } else {
-          console.log('res', res.body);
+          console.log('event err', res.body);
           return resolve(res.body);
         }
       });
   });
 }
 
-function getCalendar() {
+function getCalendar(accessToken) {
   return new Promise((resolve, reject)=>{
     request
       .get(`https://www.googleapis.com/calendar/v3/calendars/primary/events`)
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ${config.google.accessToken}`)
+      .set('Authorization', `Bearer ${accessToken}`)
       .end((err, res)=> {
         if (err) {
-          console.log('err', err)
+          //console.log('calendar err', err)
           return reject(err);
         } else {
-          console.log('res', res)
+          //console.log('calendar res', res)
           return resolve(res)
         }
-      })
-  })
+      });
+  });
 }
 
-getCalendar()
+function getAccessToken(){
+  return new Promise((resolve, reject)=>{
+    request
+      .post(`https://www.googleapis.com/oauth2/v4/token`)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Accept', 'application/json')
+      .send(`client_id=${config.google.clientId}`)
+      .send(`client_secret=${config.google.clientSecret}`)
+      .send(`refresh_token=${config.google.refreshToken}`)
+      .send(`grant_type=refresh_token`)
+      .end((err, res)=> {
+        if (err) {
+          //console.log('token err', err)
+          return reject(err);
+        } else {
+          //console.log('token res', res.body.access_token)
+          return resolve(res.body.access_token);
+        }
+      });
+  });
+}
 
-// async function updateGoogleCalendar(){
-//   //getEventData()
-//   insertGoogleEvent();
-// }
+async function updateGoogleCalendar(){
+  try {
+    const token = await getAccessToken();
+    const results = await getCalendar(token);
+    console.log(results);
+  } catch (e){
+    console.log('catch err', e);
+  }
+}
 
-//updateGoogleCalendar();
+updateGoogleCalendar();
