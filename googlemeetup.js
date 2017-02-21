257dd010b0bd45fc20dcd87734a151db8565d2b1
Sonya Pinero <sonyapinero@gmail.com>
2017-02-20 22:58:24 -0500

    format meetup event for google calendar

--- googlemeetup.js
+++ googlemeetup.js
@@ -1,6 +1,4 @@
 import request from 'superagent';
-import google from 'googleapis';
-import googleAuth from 'google-auth-library';
 import config from './config';
 
 function getEventData() {
@@ -11,41 +9,11 @@ function getEventData() {
       .set('Accept', 'application/json')
       .end((err, res) => {
         if (err) {
-          console.log('event err', err);
+          //console.log('event err', err);
           return reject(err);
         } else {
-          console.log('event err', res.body);
+          //console.log('event err', res.body);
           return resolve(res.body);
-        }
-      });
-  });
-}
-
-function insertCalendarEvent(accessToken) {
-  return new Promise((resolve, reject)=>{
-    request
-      .post(`https://www.googleapis.com/calendar/v3/calendars/primary/events`)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .send({
-        'summary': 'Test',
-        'location': '500 5th Ave',
-        'description': 'test',
-        'start': {
-          'dateTime': new Date().toISOString()
-        },
-        'end': {
-          'dateTime': new Date().toISOString()
-        }
-      })
-      .end((err, res)=> {
-        if (err) {
-          //console.log('calendar err', err)
-          return reject(err);
-        } else {
-          //console.log('calendar res', res)
-          return resolve(res)
         }
       });
   });
@@ -73,11 +41,55 @@ function getAccessToken(){
   });
 }
 
+function insertCalendarEvent(accessToken, meetupEvent) {
+  const formatEvent = {
+    'summary': meetupEvent.name,
+    'location': meetupEvent.venue.address_1,
+    'description': meetupEvent.description,
+    'start': {
+      'dateTime': new Date(meetupEvent.time).toISOString()
+    },
+    'end': {
+      'dateTime': new Date(meetupEvent.time).toISOString()
+    }
+  }
+  return new Promise((resolve, reject)=>{
+    request
+      .post(`https://www.googleapis.com/calendar/v3/calendars/primary/events`)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send(formatEvent)
+      .end((err, res)=> {
+        if (err) {
+          console.log('calendar err', err)
+          return reject(err);
+        } else {
+          console.log('calendar res', res)
+          return resolve(res)
+        }
+      });
+  });
+}
+
+function timer(ms){
+  return new Promise((resolve, reject)=>{
+    setTimeout(resolve, ms);
+  });
+};
+
 async function updateGoogleCalendar(){
   try {
+    const meetupData = await getEventData();
     const token = await getAccessToken();
-    const results = await insertCalendarEvent(token);
-    console.log(results);
+
+    (async function(){
+      for(let i = 0; i < meetupData.length; i++){
+        await insertCalendarEvent(token, meetupData[i]);
+        await timer(1000000)
+      }
+    }());
+
   } catch (e){
     console.log('catch err', e);
   }
